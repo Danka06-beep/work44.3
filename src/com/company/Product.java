15aec528981ac2e6c865e664eb77d40773c686e2
Danka02 <kamikoto02@mail.ru>
2021-07-12 12:31:01 +0300

    Initial commit

---
+++ src/com/company/Product.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.math.BigDecimal;
+
+public class Product {
+    String name;
+    BigDecimal cost;
+
+    public Product(String name, BigDecimal cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Наименование: %s, цена: %s", name, cost);
+
+    }
+}
